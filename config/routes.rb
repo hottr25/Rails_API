b482f7f58e606dfb88dd427d2e5261beffcfa235
Rob Hott <robhott48@gmail.com>
2022-10-20 12:37:20 -0400

    made comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :terms, :only => [:index, :show, :create, :update, :destroy]
   resources :types, :only => [:index, :show, :create, :update, :destroy]
 end
